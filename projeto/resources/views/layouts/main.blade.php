cc0d3b66b83ebb9c96f5902301e2db0eaaa9c4ac
brunnuscz <brunocruz012013@gmail.com>
2023-03-09 13:15:36 -0300

    Adicionando estilos as páginas

--- projeto/resources/views/layouts/main.blade.php
+++ projeto/resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>@yield('title')</title>
         
         <!-- Fonte Awesome -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">        
@@ -46,6 +46,12 @@
                 </div>
             </nav>
         </header>
+        <main>
+            @if(session('msg'))
+                <p class="msg">{{ session('msg')}}</p>
+            @endif
+            @yield('content')
+        </main>
     </body>
     
 </html>
--- projeto/resources/views/welcome.blade.php
+++ projeto/resources/views/welcome.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.main')
-@section('title', 'Homepage')
+@section('title', 'Página Inicial')
 @section('content')
+
+<div id="search-container">
+    <h1>Pesquise por uma vaga</h1>
+    <form action="/" method="GET">
+        <input type="text" name="search" id="search" class="form-control" placeholder="Pesquisar...">
+    </form>
+</div>
+
+<div class="title-vacancy col-md-12">
+    <h2>Vagas disponíveis</h2>
+</div>
+
+<div id="vacancy-container">    
+    <div id="cards-container" class="row">
+        <div class="card col-md-3">
+            <div class="card-body">
+                <h5 class="card-title">Desenvolvedor Front-end</h5>
+                <p class="card-info"><strong>Tipo da vaga: </strong>Remoto</p>
+                <p class="card-info"><strong>Nível: </strong>Júnior</p>
+                <p class="card-info"><strong>Local: </strong>Teresina</p>
+                <div class="btn-ver-mais">
+                    <a href="#" class="btn btn-primary"><i class="fa-solid fa-eye"></i> Ver mais</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
