f2d84b2cbf663d2f61270dc70e24b57338ace757
brunnuscz <brunocruz012013@gmail.com>
2023-03-10 12:42:02 -0300

    Adicionado funcionalidade de editar e deletar

--- projeto/app/Http/Controllers/VacancyController.php
+++ projeto/app/Http/Controllers/VacancyController.php
@@ -7,9 +7,11 @@ use Illuminate\Http\Request;
 /* MODEL */
 use App\Models\Vacancy;
 use App\Models\User;
+use App\Models\Candidate;
 
 class VacancyController extends Controller
 {
+    // PÁGINA INICIAL
     public function index(){
         $search = request('search');
         if($search){
@@ -21,19 +23,19 @@ class VacancyController extends Controller
         }
         return view('welcome', ['vacancies' => $vacancies, 'search' => $search]);
     }
-    
-    public function create(){
-        return view('vacancies.create');
+    // FORMULÁRIO DE CRIAR VAGA
+    public function createVacancy(){
+        return view('vacancies.create-vacancy');
     }
-
-    public function store(Request $request){
+    // CRIANDO VAGA
+    public function storeVacancy(Request $request){
         $vacancy = new Vacancy;
 
         $vacancy->title = $request->title;
         $vacancy->local = $request->local;
         $vacancy->level = $request->level;
         $vacancy->type = $request->type;
-        $vacancy->items = $request->items;
+        $vacancy->skills = $request->skills;
 
         $user = auth()->user();
         $vacancy->user_id = $user->id;
@@ -42,9 +44,31 @@ class VacancyController extends Controller
 
         return redirect('/')->with('msg', 'Vaga criada com sucesso!');
     }
+    // DASHBOARD
     public function dashboard(){
         $user = auth()->user();
+        $candidates = $user->candidates;
         $vacancies = $user->vacancies;
-        return view('vacancies.dashboard', ['vacancies' => $vacancies]);
+        return view('dashboard', ['vacancies' => $vacancies, 'candidates' => $candidates]);
+    }
+    // DELETAR VAGA
+    public function destroyVacancy($id){
+        Vacancy::findOrFail($id)->delete();
+        return redirect('/dashboard')->with('msg', 'A vaga foi excluída com sucesso!');
+    }
+    // FORMULÁRIO DE EDIÇÃO DE VAGA
+    public function editVacancy($id){
+        $user = auth()->user();
+        $vacancy = Vacancy::findOrFail($id);
+        if($user->id != $vacancy->user->id){
+            return redirect('/dashboard');
+        }
+        return view('vacancies.edit-vacancy', ['vacancy'=>$vacancy]);
+    }
+    // ATUALIZAR VAGA
+    public function updateVacancy(Request $request){
+        $data = $request->all();
+        Vacancy::findOrFail($request->id)->update($data);
+        return redirect('/dashboard')->with('msg', 'A vaga foi editda com sucesso!');
     }
 }
